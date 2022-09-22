2d23522bd67f0042e5c78afd7a3f3c393511c569
Samarjeet.S <Samarjeet.Saurabh@xnap.network>
2022-09-22 20:26:10 +0530

    added the script

---
+++ script.groovy
@@ -0,0 +1,8 @@
+import jenkins.model.Jenkins
+import hudson.model.*
+
+def version = readFile "${env.WORKSPACE}/version.txt"
+   println "${version}"
+def readcounter = version.split('\\.')
+readcounter[2]=++Integer.parseInt(readcounter[2])
+def NEXTVERSION=readcounter.join('.')

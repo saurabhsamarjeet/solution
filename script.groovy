809bb73f4ddd6b0602d3f9970d5cac291056396f
samarjeet saurabh <saurabhsamarjeet@gmail.com>
2022-09-23 15:56:20 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -3,6 +3,6 @@ import hudson.model.*
 
 def version = readFile "${env.WORKSPACE}/version.txt"
    println "${version}"
-def readcounter = version.split('\\.')
-Integer.parseInt(readcounter[2])=Integer.parseInt(readcounter[2])+1
+def readcounter = ${version}.split('\\.')
+readcounter[2]=Integer.parseInt(readcounter[2])+1
 def newversion=readcounter.join('.')

aab5a5c2982ce32583aae33297880582576ed53e
samarjeet saurabh <saurabhsamarjeet@gmail.com>
2022-09-23 15:59:29 +0530

    Update script.groovy

--- script.groovy
+++ script.groovy
@@ -6,3 +6,4 @@ def version = readFile "${env.WORKSPACE}/version.txt"
 def readcounter = version.split('\\.')
 readcounter[2]=(readcounter[2]).toInteger() +1
 def newversion=readcounter.join('.')
+println "${newversion}"
